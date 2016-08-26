6ba34865a03071d124bf7702597ec5dac930287e
Jack <zengxijin@qq.com>
2016-08-26 15:43:52 +0800

    update

--- proxy/src/main/java/com/jack/proxy/CGLIBProxy.java
+++ proxy/src/main/java/com/jack/proxy/CGLIBProxy.java
@@ -4,10 +4,20 @@ import java.lang.reflect.Method;
 
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
-
+/**
+ * @ClassName: CGLIBProxy
+ * @author:  Jack Zeng 
+ * @CreateDate: [2016年8月4日 上午10:04:03]   
+ * @UpdateUser: Jack Zeng 
+ * @UpdateDate: [2016年8月4日 上午10:04:03]   
+ * @UpdateRemark: [TODO()]
+ * @Description:  [CGLIB]
+ * @version: [V1.0]
+ */
 public class CGLIBProxy implements MethodInterceptor {
 
-	public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
+	public Object intercept(Object obj, Method method, Object[] args,MethodProxy proxy)
+			throws Throwable {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- proxy/src/main/java/com/jack/proxy/JDKProxy.java
+++ proxy/src/main/java/com/jack/proxy/JDKProxy.java
@@ -1,11 +1,25 @@
+
 package com.jack.proxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-
+/**
+ * @ClassName: JDKProxy
+ * @author:  Jack Zeng 
+ * @CreateDate: [2016年8月3日 上午11:59:23]   
+ * @UpdateUser: Jack Zeng 
+ * @UpdateDate: [2016年8月3日 上午11:59:23]   
+ * @UpdateRemark: [TODO()]
+ * @Description:  [TODO()]
+ * @version: [V1.0]
+ */
 public class JDKProxy implements InvocationHandler {
 
-	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+	
+	public String testStr = "";
+	
+	public Object invoke(Object proxy, Method method, Object[] args) 
+			throws Throwable {
 		// TODO Auto-generated method stub
 		return null;
 	}

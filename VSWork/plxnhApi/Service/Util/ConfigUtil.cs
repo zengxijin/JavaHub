63d1f1b899159689ecf4789ce9b674ea816d1239
Jack <zengxijin@qq.com>
2016-10-04 20:36:06 +0800

    json config

---
+++ VSWork/plxnhApi/Service/Util/ConfigUtil.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Util
+{
+    public class ConfigUtil
+    {
+        private static JObject json   = null;
+        private static object lockObj = new object();
+        /// <summary>
+        /// 初始化配置文件，加锁
+        /// </summary>
+        private static void loadConfig()
+        {
+            if (json == null)
+            {
+                lock (lockObj)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    using (StreamReader sr = new StreamReader("paramParse.json", Encoding.Default))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine().Trim();
+                            sb.Append(line);
+                        }
+                    }
+                    json = (JObject)JsonConvert.DeserializeObject(sb.ToString());
+                }
+            }
+        }
+
+        private static Dictionary<string, int> getJsonNode(string parentNode, string childNode)
+        {
+            loadConfig();
+
+            Dictionary<String, int> retMap = new Dictionary<string, int>();
+
+            JObject childNodeInfo = (JObject)json[parentNode][childNode];
+            foreach (JProperty jp in childNodeInfo.Properties())
+            {
+                string key = jp.Name.ToString();
+                int    val = int.Parse(jp.Value.ToString());
+                //防重复key报错，先删后加
+                retMap.Remove(key);
+                retMap.Add(key, val);
+            }
+
+            return retMap;
+        }
+
+        public static Dictionary<string,int> getRequestParamConfig(string serviceName)
+        {
+            return getJsonNode(serviceName, "request");
+        }
+
+        public static Dictionary<string, int> getResponseParamConfig(string serviceName)
+        {
+            return getJsonNode(serviceName, "response");
+        }
+    }
+}
---
+++ VSWork/plxnhApi/testConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string jsonText = "{\"beijing\":{\"zone\":\"海淀\",\"zone_en\":\"haidian\"}}";
+            //JObject jo = (JObject)JsonConvert.DeserializeObject(jsonText);
+            //string zone = jo["beijing"]["zone"].ToString();
+            //string zone_en = jo["beijing"]["zone_en"].ToString();
+
+            //Console.WriteLine(zone);
+
+            StringBuilder sb = new StringBuilder();
+            using (StreamReader sr = new StreamReader("paramParse.json", Encoding.Default))
+            {
+                
+                while(!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine().Trim();
+                    sb.Append(line);
+                }
+            }
+
+            string json = sb.ToString();
+            JObject jo = (JObject)JsonConvert.DeserializeObject(json);
+            string zone = jo["PROC_CHECK_USER"]["input"]["USER_CODE"].ToString();
+            JObject tokenList = (JObject)jo["PROC_CHECK_USER"]["input"];
+            foreach (JProperty jp in tokenList.Properties())
+            {
+               
+
+                    string key = jp.Name;
+                    string val = jp.Value.ToString();
+
+            }
+            
+            //foreach (var item in xx)
+            //{
+            //    Console.WriteLine(item);
+            //    item.
+            //}
+        }
+    }
+}

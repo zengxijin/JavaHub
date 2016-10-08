b9a27ac1c17a23bd7ee035b00399a97453297fff
Jack <zengxijin@qq.com>
2016-10-08 20:40:36 +0800

    add rules

--- VSWork/plxnhApi/testConsole/Program.cs
+++ VSWork/plxnhApi/testConsole/Program.cs
@@ -13,6 +13,30 @@ namespace testConsole
     {
         static void Main(string[] args)
         {
+            json();
+            //hhh();
+        }
+
+        public static void json()
+        {
+            string S_Returns = "0;USER_ID;USER_CODE;USER_PASS;USER_NAME;DEP_ID;USER_TEL;DEP_NAME;DEP_AREA;USER_JG;DEP_LEVEL;AREA_CODE;T_IS_FLASH_AUTHORIZED;T_YEARS;T_IS_SK;T_IS_SK_HOSP;T_IS_XJ;T_RJZ_DATE;T_CH_START_DATE;T_CH_END_DATE;T_DY_MX_IS_HZ;T_IS_BLUSH_DAY;T_BLUSH_DAY;";
+            string[] ss = S_Returns.Split(new string[] { ";" }, StringSplitOptions.None);
+
+            Dictionary<string, int> dd = hhh();
+
+            Dictionary<string, string> kk = new Dictionary<string, string>();
+
+            foreach (KeyValuePair<string, int> pair in dd)
+            {
+                if (ss.Length > pair.Value)
+                {
+                    kk.Add(pair.Key,ss[pair.Value]);
+                }
+            }
+        }
+
+        public static Dictionary<string, int> hhh()
+        {
             //string jsonText = "{\"beijing\":{\"zone\":\"海淀\",\"zone_en\":\"haidian\"}}";
             //JObject jo = (JObject)JsonConvert.DeserializeObject(jsonText);
             //string zone = jo["beijing"]["zone"].ToString();
@@ -21,10 +45,10 @@ namespace testConsole
             //Console.WriteLine(zone);
 
             StringBuilder sb = new StringBuilder();
-            using (StreamReader sr = new StreamReader("paramParse.json", Encoding.Default))
+            using (StreamReader sr = new StreamReader(@"D:\GitHub\JavaHub\VSWork\plxnhApi\Service\paramParse.json", Encoding.Default))
             {
-                
-                while(!sr.EndOfStream)
+
+                while (!sr.EndOfStream)
                 {
                     string line = sr.ReadLine().Trim();
                     sb.Append(line);
@@ -33,17 +57,18 @@ namespace testConsole
 
             string json = sb.ToString();
             JObject jo = (JObject)JsonConvert.DeserializeObject(json);
-            string zone = jo["PROC_CHECK_USER"]["input"]["USER_CODE"].ToString();
-            JObject tokenList = (JObject)jo["PROC_CHECK_USER"]["input"];
+            //string zone = jo["PROC_CHECK_USER"]["input"]["USER_CODE"].ToString();
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            JObject tokenList = (JObject)jo["PROC_CHECK_USER"]["response"];
             foreach (JProperty jp in tokenList.Properties())
             {
-               
-
-                    string key = jp.Name;
-                    string val = jp.Value.ToString();
-
+                string key = jp.Name;
+                string val = jp.Value.ToString();
+                dic.Remove(key);
+                dic.Add(key, int.Parse(val));
             }
-            
+
+            return dic;
             //foreach (var item in xx)
             //{
             //    Console.WriteLine(item);

bb59568bcd517ffd62261dcc0ece008e982bb2e6
Jack <zengxijin@qq.com>
2017-03-17 10:05:46 +0800

    add

--- drools.indepth.root/kie-server-rest-commands/src/test/java/org/jackzeng/rest/commands/RestCmdsTest.java
+++ drools.indepth.root/kie-server-rest-commands/src/test/java/org/jackzeng/rest/commands/RestCmdsTest.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.drools.core.common.DisconnectedFactHandle;
 import org.jackzeng.rest.KieServerRestCmds;
+import org.jackzeng.rest.commands.impl.RestDeleteCommand;
 import org.jackzeng.rest.commands.impl.RestStartProcessCommand;
 import org.jackzeng.rest.facts.UserInfo;
 import org.kie.api.command.Command;
@@ -14,7 +16,8 @@ public class RestCmdsTest {
 
 	public static void main(String[] args) {
 		RestCmdsTest test = new RestCmdsTest();
-		test.startProcessCmd();
+		//test.startProcessCmd();
+		test.deleteObjectCmd();
 	}
 	
 	public void startProcessCmd(){
@@ -25,6 +28,11 @@ public class RestCmdsTest {
 		List<Object> data = new ArrayList<Object>();
 		Command<?> cmd = RestStartProcessCommand.getRestJsonCmd("qf.myProcessId", parameters, "outKey", data);
 		String json = KieServerRestCmds.getCmdsInJsonWithoutBatchNode(cmd);
+		System.out.println(json);
+	}
+	
+	public void deleteObjectCmd(){
+		String json = RestDeleteCommand.getRestJson("0:1:825561880:825561880:1:DEFAULT:NON_TRAIT:com.qf.projecta.ABean", null);
 		System.out.println(json);
 	}
 

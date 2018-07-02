2498517cca2048a5769479e9f3c6ffbb7c3fe21c
xijin.zeng <zengxijin@qq.com>
2018-07-02 18:10:24 +0800

    add

--- spring-cloud-feign/src/main/java/org/jackzeng/feign/TestService.java
+++ spring-cloud-feign/src/main/java/org/jackzeng/feign/TestService.java
@@ -1,8 +1,10 @@
 package org.jackzeng.feign;
 
 import com.bkjk.credit.approvalservice.api.CreditApprovalApi;
+import com.bkjk.credit.approvalservice.data.request.quantgroup.CreditQueryRequest;
 import com.bkjk.credit.approvalservice.data.request.quantgroup.CreditSubmitRequest;
-import com.bkjk.credit.approvalservice.data.response.quantgroup.CreditSubmitResponse;
+import com.bkjk.credit.approvalservice.data.response.quantgroup.CreditQueryResponseBo;
+import com.bkjk.credit.approvalservice.data.response.quantgroup.CreditSubmitResponseBo;
 import com.bkjk.credit.datacollection.api.DataCollectionApi;
 import com.bkjk.credit.datacollection.domain.FetchRelativeRecordRequestBo;
 import com.bkjk.credit.datacollection.domain.FetchRelativeRecordResponseBo;
@@ -33,37 +35,53 @@ public class TestService {
     }
 
     public void testApprovalApi() throws IOException {
-        String json = "{\n" +
-                "\t\"ssn\": \"310108199003208492\",\n" +
-                "\t\"name\": \"王丽\",\n" +
-                "\t\"telephone\": \"13810001000\",\n" +
-                "\t\"channelId\": \"CREDIT_APPLICATION\",\n" +
-                "\t\"requestId\": \"15\",\n" +
-                "\t\"timestamp\": " + System.currentTimeMillis() + ",\n" +
-                "\t\"channelData\": {\n" +
-                "\t\t\"userId\": \"string4545447\",\n" +
-                "\t\t\"bankcardType\": \"debitcard\",\n" +
-                "\t\t\"bankcardNo\": \"6222000002204995655\",\n" +
-                "\t\t\"minBudget\": 3000.0,\n" +
-                "\t\t\"maxBudget\": 6000.0,\n" +
-                "\t\t\"isFreshGraduate\": true,\n" +
-                "\t\t\"rentalCity\": \"上海\",\n" +
-                "\t\t\"occupation\": \"STUDENT\",\n" +
-                "\t\t\"school\": \"上海交通大学\",\n" +
-                "\t\t\"graduationDate\": 1526295138025,\n" +
-                "\t\t\"diplomaNo\": \"string\",\n" +
-                "\t\t\"company\": \"string\",\n" +
-                "\t\t\"jobPosition\": \"高级开发工程师\",\n" +
-                "\t\t\"monthlySalary\": 10000.0\n" +
-                "\t}\n" +
+        String json ="{\n" +
+                "       \"timestamp\": 1527245567466,               \n" +
+                "\t\t\"name\":\"杨瑞平\",\n" +
+                "\t\t\"ssn\":\"520221198403073011\",\n" +
+                "\t    \"telephone\" : \"18216826522\",\n" +
+                "\t    \"channelId\" : \"CREDIT_APPLICATION\",\n" +
+                "\t    \"requestId\" : \"test_o04\",                   \n" +
+                "    \t\"channelData\":{\n" +
+                "\t        \"userId\" : \"test_o04\",\n" +
+                "\t        \"bankcardType\" : \"debitcard\",\n" +
+                "\t        \"bankcardNo\" : \"621785526620147001\",\n" +
+                "\t        \"minBudget\" : 1000,\n" +
+                "\t        \"maxBudget\" : 2000 ,\n" +
+                "\t        \"isFreshGraduate\" : true,\n" +
+                "\t        \"rentalCity\" : \"上海\",\n" +
+                "\t        \"occupation\" : \" STUDENT \",\n" +
+                "\t        \"school\" : \"上海交通大学1\",            \n" +
+                "\t        \"graduationDate\": 1514764800001,              \n" +
+                "\t        \"diplomaNo\" : \"1\",\n" +
+                "\t        \"company\" : \"1\",\n" +
+                "\t        \"jobPosition\" : \"高级开发工程师\",\n" +
+                "\t        \"monthlySalary\" : 10001.0                \n" +
+                "       }\n" +
                 "}";
         ObjectMapper mapper = new ObjectMapper();
 
         CreditSubmitRequest request = mapper.readValue(json, CreditSubmitRequest.class);
         System.out.println(request);
 
-        CreditSubmitResponse response = creditApprovalApi.submitCreditRequest(request);
+        CreditSubmitResponseBo response = creditApprovalApi.submitCreditRequest(request);
         System.out.println(mapper.writeValueAsString(response));
+
+        String json2 = "{\n" +
+                "\t\"name\": \"钱斌\",\n" +
+                "    \"ssn\": \"469001199811128114\",\n" +
+                "    \"telephone\": \"18621114725\",\n" +
+                "\t\"timestamp\": 1527245567466,\n" +
+                "\t\"channelId\": \"CREDIT_APPLICATION\",\n" +
+                "\t\"requestId\": \"test_o04\",\n" +
+                "\t\"channelData\": {\n" +
+                "\t\t\"userId\": \"test_o04\"\n" +
+                "\t}\n" +
+                "}";
+
+        CreditQueryRequest request2 = mapper.readValue(json2, CreditQueryRequest.class);
+        CreditQueryResponseBo creditQueryResponseBo = creditApprovalApi.queryCreditResult(request2);
+        System.out.println(mapper.writeValueAsString(creditQueryResponseBo));
 
     }
 }

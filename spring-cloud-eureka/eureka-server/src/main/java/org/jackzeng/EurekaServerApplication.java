daa10634ab418ce2204ce8e2cd5e4a10e6e30ffd
xijin.zeng <zengxijin@qq.com>
2019-05-17 19:34:38 +0800

    add eureka server

---
+++ spring-cloud-eureka/eureka-server/src/main/java/org/jackzeng/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package org.jackzeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xijin.zeng created on 2019/5/17
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}

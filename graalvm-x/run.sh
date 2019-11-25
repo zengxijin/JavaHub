5b53a31bb29e6ae24ccc8fd838cf27ba1603dee6
xijin.zeng <zengxijin@qq.com>
2019-11-25 19:00:54 +0800

    graalvm python test

---
+++ graalvm-x/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PATH=/Users/zengxijin/graalvm-ce-java11-19.3.0/Contents/Home/bin:$PATH
+java -jar target/graalvm-x-1.0-SNAPSHOT.jar
---
+++ graalvm-x/src/main/java/graalvm/x/python/PyFunction.java
@@ -0,0 +1,8 @@
+package graalvm.x.python;
+
+/**
+ * @author zengxijin created on 2019-11-25
+ */
+public class PyFunction {
+
+}
